6e3e14941ce86bcf9dd43311e50cdb80fa2eb2d2
Shreyas Sail <44506006+shreyas-sail@users.noreply.github.com>
2022-03-16 21:49:35 +0530

    Time: 2 ms (73.78%), Space: 55.4 MB (8.26%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length<=1) return;
+        int l =0,r =1;
+        while(l<nums.length && r<nums.length){
+            if(nums[l] == 0 && nums[r]!=0){
+                int temp =nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;r++;
+            }else if(nums[l] != 0 && nums[r]==0){
+                l++;
+            }else {
+                r++;
+            }
+        }
+    }
+}
