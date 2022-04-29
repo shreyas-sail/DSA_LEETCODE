110f01847219cdba815949389791d793e95f9f2f
Shreyas Sail <44506006+shreyas-sail@users.noreply.github.com>
2022-04-29 12:52:34 +0530

    Time: 1 ms (41.59%), Space: 41.6 MB (15.42%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int curRow = 0,curCol = 0;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			dp[i][j] = -1;
+        int totalPaths = traverseRecursive(curRow,curCol,m,n,dp);
+        return totalPaths;
+    }
+    
+    public int traverseRecursive(int r,int c,int m,int n,int[][] dp){
+        if(r>m-1 || c>n-1){
+            return 0;
+        }
+        if(r == m-1 && c == n-1){
+            return 1;
+        }
+        if (dp[r][c] != -1) return dp[r][c];
+        int downWards = traverseRecursive(r+1,c,m,n,dp);
+        int upWards = traverseRecursive(r,c+1,m,n,dp);
+        return dp[r][c] =  downWards + upWards; 
+    }
+}
