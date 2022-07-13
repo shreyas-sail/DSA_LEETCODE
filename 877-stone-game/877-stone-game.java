082c676e19e98acffa46defc91224260d796a0e2
Shreyas Sail <44506006+shreyas-sail@users.noreply.github.com>
2022-07-13 23:33:11 +0530

    Time: 10 ms (51.96%), Space: 47.7 MB (42.74%) - LeetHub

--- 877-stone-game/877-stone-game.java
+++ 877-stone-game/877-stone-game.java
@@ -5,26 +5,29 @@ class Solution {
         int n = piles.length;
         int i = 0;
         int j = n-1;
-        // System.out.println(stoneHelper(i,j,piles,true));
-        // System.out.println(stoneHelper(i,j,piles,false));
-        boolean pickFirst = stoneHelper(i,j,piles,true);
+        int[][] dp = new int[n][n];
+        for(int[] d:dp)
+        Arrays.fill(d,-1);
+        boolean pickFirst = stoneHelper(i,j,piles,true,dp);
         aScore = 0;bScore = 0;
-        boolean pickLast = stoneHelper(i,j,piles,false);
+        for(int[] d:dp)
+        Arrays.fill(d,-1);
+        boolean pickLast = stoneHelper(i,j,piles,false,dp);
         return pickFirst || pickLast;
     }
-    boolean stoneHelper(int i,int j,int[] piles,boolean isAliceTurn){
+    boolean stoneHelper(int i,int j,int[] piles,boolean isAliceTurn,int[][] dp){
         if(i>j){
-            // System.out.printf("%d %d\n",aScore,bScore);
             return aScore>bScore ;
         }
-        
+        if(dp[i][j] !=-1) return dp[i][j] == 1;
         if(isAliceTurn){
              aScore += piles[i];
         }else{
             bScore += piles[i];
         }
-        boolean pickFirst = stoneHelper(i+1,j,piles,!isAliceTurn);
-        return aScore>bScore ; 
+        boolean pickFirst = stoneHelper(i+1,j,piles,!isAliceTurn,dp);
+        dp[i][j] = aScore>bScore?1:0;
+        return dp[i][j] == 1 ; 
     }
     
     
