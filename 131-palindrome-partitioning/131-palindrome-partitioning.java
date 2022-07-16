377f8e9fd981aeb365390fb80f31ab6fe282fc31
Shreyas Sail <44506006+shreyas-sail@users.noreply.github.com>
2022-07-16 08:57:23 +0530

    Time: 15 ms (78.15%), Space: 192.7 MB (51.18%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -6,16 +6,12 @@ class Solution {
         return ans;
     }
     void backtrack(int index,List<List<String>> ans,List<String> res,String s,int n){
-        System.out.println(index);
         if(n == index){
             ans.add(new ArrayList<String>(res));
             return;
         }
-        
         for(int i = index;i<n;i++){
-            // System.out.println(isPali(s,index,i));
             if(isPali(s,index,i)){
-                // System.out.println("yes");
                 res.add(s.substring(index,i+1));
                 backtrack(i+1,ans,res,s,n);
                 res.remove(res.size()-1);
