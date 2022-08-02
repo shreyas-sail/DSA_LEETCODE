c116a510c9dd6e18bcdee976042e668f3b967391
Shreyas Sail <44506006+shreyas-sail@users.noreply.github.com>
2022-08-03 00:09:50 +0530

    Time: 1 ms (92.44%), Space: 57.8 MB (12.68%) - LeetHub

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+       // if (matrix == null || k <= 0) {
+       //      throw new IllegalArgumentException("Input is invalid");
+       //  }
+
+        int n = matrix.length;
+        // if (k > n * n) {
+        //     throw new NoSuchElementException("k is greater than number of elements in matrix");
+        // }
+        if (k == 1) {
+            return matrix[0][0];
+        }
+        if (k == n * n) {
+            return matrix[n - 1][n - 1];
+        }
+
+        int start = matrix[0][0];
+        int end = matrix[n - 1][n - 1];
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int[] smallLargePair = { start, end };
+            int count = countLessThanAndEqual(matrix, mid, smallLargePair);
+            if (count == k) {
+                return smallLargePair[0];
+            }
+            if (count < k) {
+                start = smallLargePair[1]; // search higher
+            } else {
+                end = smallLargePair[0]; // search lower
+            }
+        }
+
+        return start;
+    }
+
+    private int countLessThanAndEqual(int[][] matrix, int mid, int[] smallLargePair) {
+        int count = 0;
+        int n = matrix.length;
+        int row = 0;
+        int col = n - 1;
+        while (row < n && col >= 0) {
+            if (matrix[row][col] > mid) {
+                // as matrix[row][col] is bigger than the mid, let's keep track of the
+                // smallest number greater than the mid
+                smallLargePair[1] = Math.min(smallLargePair[1], matrix[row][col]);
+                col--;
+            } else {
+                // as matrix[row][col] is less than or equal to the mid, let's keep track of the
+                // biggest number less than or equal to the mid
+                smallLargePair[0] = Math.max(smallLargePair[0], matrix[row][col]);
+                row++;
+                count += col + 1;
+            }
+        }
+        return count;
+    } 
+    
+}
