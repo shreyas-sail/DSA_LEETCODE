9ac3e7279caf9675a99f621949f3b76b117208a2
Shreyas Sail <44506006+shreyas-sail@users.noreply.github.com>
2022-08-12 22:10:31 +0530

    Time: 176 ms (41.83%), Space: 132.7 MB (35.07%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -4,8 +4,6 @@ class Solution {
         int n = arr.length;
         int[] ans = new int[n];
         for(int i =n-1;i>=0;i--){
-            // [89,62,70,58,47,47,46,76,100,70]
-            // [               ,1 ,1,,0, 0 ] 
             while(!st.isEmpty() && arr[st.peek()]<=arr[i]){
                 st.pop();
             }
