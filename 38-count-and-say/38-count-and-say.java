00826a2e89250cf4772efa5a680a37e794d02140
Shreyas Sail <44506006+shreyas-sail@users.noreply.github.com>
2022-08-22 23:02:51 +0530

    Time: 50 ms (16.45%), Space: 62.7 MB (15.06%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -4,7 +4,6 @@ class Solution {
         String temp = "";
         for(int i = 2;i<=n;i++){
             char[] c = s.toCharArray();
-            System.out.println(s);
             temp = "";
             for(int j = 0;j<c.length;j++){
                 int count = 1;
@@ -12,10 +11,7 @@ class Solution {
                 
                 while(j+1<c.length && c[j] == c[j+1]){ //111221 
                     j++;count++;
-                    // System.out.println(c.length);
                 }
-                // System.out.println(count);
-                // if(count>1) j-=1;
                 temp += String.valueOf(count)+cur;
             }
             s = temp;
